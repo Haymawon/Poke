e050076dc8ae83cb9e5af07c7f04ec536b0ad4e7
Haymawon <haymawon009@gmail.com>
2025-09-03 08:11:25 +0630

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1616,4 +1616,24 @@
                 }
             });
         }
+
     });
+
+let originalTitle = document.title;
+let blinkInterval;
+
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    // Start blinking title
+    blinkInterval = setInterval(() => {
+      document.title =
+        document.title === "Wait! Come back!"
+          ? originalTitle
+          : "Wait! Come back!";
+    }, 1000); // change every 1 second
+  } else {
+    // Stop blinking and reset title
+    clearInterval(blinkInterval);
+    document.title = originalTitle;
+  }
+});
